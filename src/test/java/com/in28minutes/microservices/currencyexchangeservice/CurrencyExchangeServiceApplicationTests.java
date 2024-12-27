5ca442cbcea1fc2d1a80c18e65571c04c4799f28
Satyajeet Deshmukh <sdeshmukh@mobiquityinc.com>
2024-12-27 12:09:57 +0530

    Docker-version:  currency-exchange-service

---
+++ src/test/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CurrencyExchangeServiceApplication.class)
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
